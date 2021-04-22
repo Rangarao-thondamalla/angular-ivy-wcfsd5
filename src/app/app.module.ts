139b35ce4c71844abef26446353f87ec4c0fba59
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-04-22 21:28:07 +0530

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {DemoMaterialModule} from './material-module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { InputOverviewExampleComponent } from './input-overview-example/input-overview-example.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule , MatNativeDateModule,DemoMaterialModule,BrowserAnimationsModule ],
+  entryComponents: [InputOverviewExampleComponent],
+  declarations: [ AppComponent, HelloComponent, InputOverviewExampleComponent ],
+  bootstrap:    [ AppComponent ],
+   providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ]
 })
 export class AppModule { }
