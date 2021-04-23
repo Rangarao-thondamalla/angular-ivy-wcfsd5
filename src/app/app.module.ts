1481432199f6bda3ad4d84bf9a8eaef3120385fe
Rangarao-thondamalla <t.rangarao007@gmail.com>
2021-04-23 20:41:54 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {DemoMaterialModule} from './material-module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { DemoMaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
 
@@ -12,7 +13,16 @@ import { HelloComponent } from './hello.component';
 import { InputOverviewExampleComponent } from './input-overview-example/input-overview-example.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ,ReactiveFormsModule , MatNativeDateModule,DemoMaterialModule,BrowserAnimationsModule ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule ,
+    ReactiveFormsModule , 
+    MatNativeDateModule,
+    DemoMaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+     ],
+
   entryComponents: [InputOverviewExampleComponent],
   declarations: [ AppComponent, HelloComponent, InputOverviewExampleComponent ],
   bootstrap:    [ AppComponent ],
